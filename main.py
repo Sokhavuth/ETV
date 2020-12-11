96aceccff427729dfff3581f7d76d6e40c299314
Sokhavuth TIN <vuthdevelop@gmail.com>
2020-12-11 21:45:48 +0700

    Make it better

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from controllers.index import Index
 app = Index()
      
 if 'DYNO' in os.environ:
-  app.run(server('gunicorn'), host='0.0.0.0', port=os.environ.get('PORT', 9000))
+  app.run(host='0.0.0.0', port=os.environ.get('PORT', 9000))
 else: 
   app.run(host='localhost', port=9000, debug=True, reloader=True)
