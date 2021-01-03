134e6764c41adf1794c9666d3deafef0e792df48
Sokhavuth TIN <vuthdevelop@gmail.com>
2021-01-03 10:14:15 +0700

    Make it better

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -1,4 +1,5 @@
 #routes/dashboard.py
+import asyncio
 from flask_classful import FlaskView, route
 from controllers.dashboard.user import User
 from controllers.dashboard.index import Index
@@ -7,64 +8,74 @@ from controllers.dashboard.series import Series
 
 class Dashboard(FlaskView):
   def __init__(self):
-    #super().__init__()
-    self.user = User()
-    self.index = Index()
-    self.movie = Movie()
-    self.series = Series()
-
+    super().__init__()
+    
   @route('/')
   def dashboard_index(self):
+    self.index = Index()
     return self.index.index()
 
   @route('/logout/')
   def dashboard_logout(self):
+    self.index = Index()
     return self.index.logout()
     
   @route('/user/signup/', methods=['GET', 'POST'])
   def dashboard_signup(self):
+    self.user = User()
     return self.user.signup()
 
   @route('/user/edit/<id>')
   def dashboard_edit(self, id):
+    self.user = User()
     return self.user.edit(id)
 
   @route('/user/delete/<id>')
   def dashboard_delete(self, id):
+    self.user = User()
     return self.user.delete(id)
 
   @route('/user/load/')
   def load_user(self):
+    self.user = User()
     return self.user.load()
 
   @route('/movie/', methods=['GET', 'POST'])
   def dashboard_movie(self):
+    self.movie = Movie()
     return self.movie.get_post()
 
   @route('/movie/delete/<id>')
   def dashboard_movie_delete(self, id):
+    self.movie = Movie()
     return self.movie.delete(id)
 
   @route('/movie/edit/<id>')
   def dashboard_movie_edit(self, id):
+    self.movie = Movie()
     return self.movie.edit(id)
 
   @route('/movie/load/')
   def dashboard_movie_load(self):
+    self.movie = Movie()
     return self.movie.load()
 
   @route('/series/', methods=['GET', 'POST'])
   def dashborad_series(self):
+    self.series = Series()
     return self.series.get_post()
 
   @route('/series/delete/<id>')
   def dashboard_series_delete(self, id):
+    self.series = Series()
     return self.series.delete(id)
 
   @route('/series/edit/<id>')
   def dashboard_series_edit(self, id):
+    self.series = Series()
     return self.series.edit(id)
 
   @route('/series/load/')
   def dashboard_series_load(self):
+    self.series = Series()
     return self.series.load()
--- routes/index.py
+++ routes/index.py
@@ -1,5 +1,5 @@
 #routes/index.py
-import config, copy
+import asyncio
 from flask import render_template
 from flask_classful import FlaskView, route
 from controllers.login import Login
@@ -11,20 +11,16 @@ from controllers.country_series import Country_series
  
 class Index(FlaskView):
   def __init__(self):
-    #super().__init__()
-    self.login = Login()
-    self.home = Home()
-    self.movie = Movie()
-    self.country = Country()
-    self.series = Series()
-    self.country_series = Country_series()
+    super().__init__()
  
   @route('/')
   def index(self):
+    self.home = Home()
     return self.home.get_youtube()
-
+    
   @route('/login/', methods=['GET', 'POST'])
   def check_user(self):
+    self.login = Login()
     return self.login.check_user()
 
   @route('/user/<id>')
@@ -33,24 +29,30 @@ class Index(FlaskView):
 
   @route('/movie/<id>')
   def get_movie(self, id):
+    self.movie = Movie()
     return self.movie.get_movie(id)
 
   @route('/movie/country/<label>')
   def get_category(self, label):
+    self.country = Country()
     return self.country.get_movie(label)
 
   @route('/movie/country/load/')
   def load_movie(self):
+    self.country = Country()
     return self.country.load_movie()
 
   @route('/series/<id>')
   def get_series(self, id):
+    self.series = Series()
     return self.series.get_series(id)
 
   @route('/series/country/<label>')
   def get_category_series(self, label):
+    self.country_series = Country_series()
     return self.country_series.get_series(label)
 
   @route('/series/country/load/')
   def load_series(self):
+    self.country_series = Country_series()
     return self.country_series.load_series()
