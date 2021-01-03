449de5f45d5f961c69617617b8f5185712612aa9
Sokhavuth TIN <vuthdevelop@gmail.com>
2021-01-03 07:10:13 +0700

    Make it better

--- static/scripts/series.js
+++ static/scripts/series.js
@@ -44,6 +44,9 @@ class Series{
       var width = $('#series-wrapper').css('width');
       var height = parseInt(width)/16*9;
       $('#series-wrapper').css('min-height', height);
+      if($('.fb-video') && (fb_api)){
+        FB.XFBML.parse();
+      }
     });
 
     $('#series-wrapper').html(iframe);
